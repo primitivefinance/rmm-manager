69fa2e530c4f18ab4455bb5526acdd4cd4ae85c4
Kinrezc <matt.czernik@gmail.com>
2021-07-09 15:17:25 -0400

    fix(callbacks): assert msg.sender == engine in all callbacks

--- contracts/interfaces/IPrimitiveHouse.sol
+++ contracts/interfaces/IPrimitiveHouse.sol
@@ -19,16 +19,14 @@ interface IPrimitiveHouse is
     // init
     function initialize(address engine_) external;
     // Margin
-    function create(uint256 strike, uint64 sigma, uint32 time, uint256 riskyPrice, bytes calldata data) external;
-    function deposit(address owner, uint delRisky, uint delStable, bytes calldata data) external;
+    function create(uint256 strike, uint64 sigma, uint32 time, uint256 riskyPrice) external;
+    function deposit(address owner, uint delRisky, uint delStable) external;
     function withdraw(uint256 delRisky, uint256 delStable) external;
-    function borrow(bytes32 poolId, address owner, uint delLiquidity, uint256 maxPremium, bytes calldata data) external;
-    function allocate(bytes32 poolId, address owner, uint256 delLiquidity, bool fromMargin, bytes calldata data) external;
-    function repay(bytes32 poolId, address owner, uint256 delLiquidity, bool fromMargin, bytes calldata data) external;
+    function borrow(bytes32 poolId, address owner, uint delLiquidity, uint256 maxPremium) external;
+    function allocate(bytes32 poolId, address owner, uint256 delLiquidity, bool fromMargin) external;
+    function repay(bytes32 poolId, address owner, uint256 delLiquidity, bool fromMargin) external;
     // Swap
-    function swap(bytes32 poolId, bool addXRemoveY, uint256 deltaOut, uint256 maxDeltaIn, bytes calldata data) external;
+    function swap(bytes32 poolId, bool addXRemoveY, uint256 deltaOut, uint256 maxDeltaIn, bytes memory data) external;
     function swapXForY(bytes32 poolId, uint deltaOut) external;
     function swapYForX(bytes32 poolId, uint deltaOut) external;
-    // Lending
-    function margins(address owner) external view returns (Margin.Data memory);
 }
