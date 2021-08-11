51ab61833d58f6a71963304b59f0da5e3654b22c
Clement Lakhal <clementlakhal@gmail.com>
2021-08-11 23:26:59 +0200

    feat: add PositionSVG

---
+++ contracts/libraries/PositionSVG.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.6;
+
+/// @title   PositionSVG
+/// @author  Primitive
+/// @dev     Generates an SVG image from a position
+///          Work in progress, this library might change!
+
+library PositionSVG {
+    function generateSVG() internal pure returns (string memory) {
+        return "";
+    }
+
+    /*
+    function tokenURI(uint256 tokenId) public pure override returns (string memory) {
+        tokenId;
+        return 'data:text/plain,{"name":"Name goes here","description":"Description goes here","image":"data:image/svg+xml,<svg width=\'300\' height=\'300\' xmlns=\'http://www.w3.org/2000/svg\'><path d=\'M50 50h50v50z\'/></svg>"}';
+    }
+    */
+}
