6d4880a72dede0be1e9c0ec346459760c9d37dc9
Clement Lakhal <clementlakhal@gmail.com>
2021-08-18 00:38:15 +0200

    feat: add base folder

--- contracts/base/Whitelist.sol
+++ contracts/base/Whitelist.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.6;
 
-import "./interfaces/IWhitelist.sol";
+import "../interfaces/IWhitelist.sol";
 import "./Admin.sol";
 
 contract Whitelist is IWhitelist, Admin {
