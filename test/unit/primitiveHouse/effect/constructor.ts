8103982b1adbfa85b192836990c4cd1ce5520a5c
clemlak <clementlakhal@gmail.com>
2021-08-30 19:22:13 +0200

    test: update constructor test

--- test/unit/primitiveHouse/effect/constructor.ts
+++ test/unit/primitiveHouse/effect/constructor.ts
@@ -11,5 +11,9 @@ describe('constructor', function () {
     it('sets the address of the factory', async function () {
       expect(await this.house.factory()).to.equal(this.factory.address)
     })
+
+    it('sets the address of WETH10', async function () {
+      expect(await this.house.WETH10()).to.equal('0x4f5704D9D2cbCcAf11e70B34048d41A0d572993F')
+    })
   })
 })
