531e5da508d7d3ee8896b3e33eb48819a4145f56
clemlak <clementlakhal@gmail.com>
2021-08-30 18:19:39 +0200

    feat: add SelfPermit

---
+++ contracts/interfaces/ISelfPermit.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.6;
+
+interface ISelfPermit {
+    function selfPermit(
+        address token,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external payable;
+
+    function selfPermitIfNecessary(
+        address token,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external payable;
+}
