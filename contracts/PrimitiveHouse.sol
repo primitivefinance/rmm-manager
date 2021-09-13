48356706e6c38141950c40c3e675f2a1cb1fa775
Clement Lakhal <clementlakhal@gmail.com>
2021-09-13 18:12:31 +0400

    feat: move deposit / withdraw to margin manager

--- contracts/PrimitiveHouse.sol
+++ contracts/PrimitiveHouse.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.6;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@primitivefinance/v2-core/contracts/interfaces/engine/IPrimitiveEngineActions.sol";
 import "@primitivefinance/v2-core/contracts/interfaces/engine/IPrimitiveEngineView.sol";
 
 import "./interfaces/IPrimitiveHouse.sol";
@@ -28,9 +27,9 @@ contract PrimitiveHouse is
     MarginManager,
     SwapManager
 {
+    using SafeERC20 for IERC20;
     using Margin for mapping(address => Margin.Data);
     using Margin for Margin.Data;
-    using SafeERC20 for IERC20;using SafeERC20 for IERC20;
 
     /// EFFECT FUNCTIONS ///
 
@@ -79,68 +78,6 @@ contract PrimitiveHouse is
         _allocate(msg.sender, engine, poolId, delLiquidity - 1000);
 
         emit Created(msg.sender, engine, poolId, strike, sigma, maturity);
-    }
-
-    struct DepositCallbackData {
-        address payer;
-        address risky;
-        address stable;
-        uint256 delRisky;
-        uint256 delStable;
-    }
-
-    /// @inheritdoc IPrimitiveHouse
-    function deposit(
-        address recipient,
-        address risky,
-        address stable,
-        uint256 delRisky,
-        uint256 delStable
-    ) public virtual override lock {
-        // TODO: Revert if delRisky || delStable == 0?
-        address engine = factory.getEngine(risky, stable);
-
-        IPrimitiveEngineActions(engine).deposit(
-            address(this),
-            delRisky,
-            delStable,
-            abi.encode(
-                DepositCallbackData({
-                    payer: msg.sender,
-                    risky: risky,
-                    stable: stable,
-                    delRisky: delRisky,
-                    delStable: delStable
-                })
-            )
-        );
-
-        Margin.Data storage mar = margins[engine][recipient];
-        mar.deposit(delRisky, delStable);
-
-        emit Deposited(msg.sender, recipient, engine, risky, stable, delRisky, delStable);
-    }
-
-    /// @inheritdoc IPrimitiveHouse
-    function withdraw(
-        address recipient,
-        address risky,
-        address stable,
-        uint256 delRisky,
-        uint256 delStable
-    ) public virtual override lock {
-        if (delRisky == 0 || delStable == 0) {
-            // TODO: Revert the call or not?
-        }
-
-        address engine = factory.getEngine(risky, stable);
-
-        // Reverts the call early if margins are insufficient
-        margins[engine].withdraw(delRisky, delStable);
-
-        IPrimitiveEngineActions(engine).withdraw(recipient, delRisky, delStable);
-
-        emit Withdrawn(msg.sender, recipient, engine, delRisky, delStable);
     }
 
     struct AllocateCallbackData {
@@ -213,19 +150,6 @@ contract PrimitiveHouse is
         bytes calldata data
     ) external override {
         CreateCallbackData memory decoded = abi.decode(data, (CreateCallbackData));
-
-        if (msg.sender != factory.getEngine(decoded.risky, decoded.stable)) revert NotEngineError();
-
-        if (delRisky > 0) IERC20(decoded.risky).safeTransferFrom(decoded.payer, msg.sender, delRisky);
-        if (delStable > 0) IERC20(decoded.stable).safeTransferFrom(decoded.payer, msg.sender, delStable);
-    }
-
-    function depositCallback(
-        uint256 delRisky,
-        uint256 delStable,
-        bytes calldata data
-    ) external override {
-        DepositCallbackData memory decoded = abi.decode(data, (DepositCallbackData));
 
         if (msg.sender != factory.getEngine(decoded.risky, decoded.stable)) revert NotEngineError();
 
--- contracts/base/MarginManager.sol
+++ contracts/base/MarginManager.sol
@@ -1,10 +1,92 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.6;
 
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@primitivefinance/v2-core/contracts/interfaces/engine/IPrimitiveEngineActions.sol";
 import "@primitivefinance/v2-core/contracts/libraries/Margin.sol";
 
 import "../interfaces/IPrimitiveHouse.sol";
+import "./HouseBase.sol";
 
-abstract contract MarginManager is IPrimitiveHouse {
+abstract contract MarginManager is IPrimitiveHouse, HouseBase {
+    using Margin for mapping(address => Margin.Data);
+    using Margin for Margin.Data;
+    using SafeERC20 for IERC20;
+
     mapping(address => mapping(address => Margin.Data)) public override margins;
+
+    struct DepositCallbackData {
+        address payer;
+        address risky;
+        address stable;
+        uint256 delRisky;
+        uint256 delStable;
+    }
+
+    /// @inheritdoc IPrimitiveHouse
+    function deposit(
+        address recipient,
+        address risky,
+        address stable,
+        uint256 delRisky,
+        uint256 delStable
+    ) public virtual override lock {
+        // TODO: Revert if delRisky || delStable == 0?
+        address engine = factory.getEngine(risky, stable);
+
+        IPrimitiveEngineActions(engine).deposit(
+            address(this),
+            delRisky,
+            delStable,
+            abi.encode(
+                DepositCallbackData({
+                    payer: msg.sender,
+                    risky: risky,
+                    stable: stable,
+                    delRisky: delRisky,
+                    delStable: delStable
+                })
+            )
+        );
+
+        Margin.Data storage mar = margins[engine][recipient];
+        mar.deposit(delRisky, delStable);
+
+        emit Deposited(msg.sender, recipient, engine, risky, stable, delRisky, delStable);
+    }
+
+    /// @inheritdoc IPrimitiveHouse
+    function withdraw(
+        address recipient,
+        address risky,
+        address stable,
+        uint256 delRisky,
+        uint256 delStable
+    ) public virtual override lock {
+        if (delRisky == 0 || delStable == 0) {
+            // TODO: Revert the call or not?
+        }
+
+        address engine = factory.getEngine(risky, stable);
+
+        // Reverts the call early if margins are insufficient
+        margins[engine].withdraw(delRisky, delStable);
+
+        IPrimitiveEngineActions(engine).withdraw(recipient, delRisky, delStable);
+
+        emit Withdrawn(msg.sender, recipient, engine, delRisky, delStable);
+    }
+
+    function depositCallback(
+        uint256 delRisky,
+        uint256 delStable,
+        bytes calldata data
+    ) external override {
+        DepositCallbackData memory decoded = abi.decode(data, (DepositCallbackData));
+
+        if (msg.sender != factory.getEngine(decoded.risky, decoded.stable)) revert NotEngineError();
+
+        if (delRisky > 0) IERC20(decoded.risky).safeTransferFrom(decoded.payer, msg.sender, delRisky);
+        if (delStable > 0) IERC20(decoded.stable).safeTransferFrom(decoded.payer, msg.sender, delStable);
+    }
 }
