7de0ccb69c418fb52abc193a91eed104bc00a38c
alex <alexangel.j@gmail.com>
2021-09-14 16:50:46 -0700

    fix(swap-test): stable swaps working ser

---
+++ contracts/test/MockEngine.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.6;
+
+import "@primitivefinance/v2-core/contracts/PrimitiveEngine.sol";
+
+contract MockEngine is PrimitiveEngine {
+    uint256 public time = 1;
+
+    function advanceTime(uint256 by) external {
+        time += by;
+    }
+
+    function _blockTimestamp() internal view override returns (uint32 blockTimestamp) {
+        blockTimestamp = uint32(time);
+    }
+
+    function setReserves(
+        bytes32 poolId,
+        uint256 reserveRisky,
+        uint256 reserveStable
+    ) public {
+        Reserve.Data storage res = reserves[poolId];
+        res.reserveRisky = SafeCast.toUint128(reserveRisky);
+        res.reserveStable = SafeCast.toUint128(reserveStable);
+    }
+}
