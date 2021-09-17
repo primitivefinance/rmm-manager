0d9c308aca66ffd59adaed36ff9fe43a77e081ec
Clement Lakhal <clementlakhal@gmail.com>
2021-09-17 09:50:21 +0400

    feat: add immutable and interface

---
+++ contracts/interfaces/IHouseBase.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.6;
+
+
+interface IHouseBase {
+    function factory() external returns (address);
+    function WETH10() external returns (address);
+}
