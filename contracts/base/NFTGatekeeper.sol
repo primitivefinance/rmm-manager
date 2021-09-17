abc7ea8002983627c73952f92daabc2c634b6e0a
Clement Lakhal <clementlakhal@gmail.com>
2021-09-17 09:50:29 +0400

    feat: add immutable

--- contracts/base/NFTGatekeeper.sol
+++ contracts/base/NFTGatekeeper.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "../interfaces/INFTGatekeeper.sol";
 
 contract NFTGatekeeper is INFTGatekeeper {
-    uint256 public override id;
-    address public override token;
+    uint256 public immutable override id;
+    address public immutable override token;
 
     modifier onlyHolder() {
         if (IERC1155(token).balanceOf(msg.sender, id) == 0) {
