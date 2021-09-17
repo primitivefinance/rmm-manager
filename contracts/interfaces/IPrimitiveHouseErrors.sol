6d5616db7983ff2333890e28c6ab79a575a42d9d
Clement Lakhal <clementlakhal@gmail.com>
2021-09-17 15:43:10 +0400

    feat: update interface (events, errors, funcs)

--- contracts/interfaces/IPrimitiveHouseErrors.sol
+++ contracts/interfaces/IPrimitiveHouseErrors.sol
@@ -2,12 +2,6 @@
 pragma solidity 0.8.6;
 
 interface IPrimitiveHouseErrors {
-    /// @notice Thrown when the delta out is lower than the minimum
-    /// @param expected The minimum delta out
-    /// @param actual The actual delta out
-    error DeltaOutMinError(uint256 expected, uint256 actual);
-
     error EngineNotDeployedError();
-
     error ZeroLiquidityError();
 }
--- contracts/interfaces/ISwapManager.sol
+++ contracts/interfaces/ISwapManager.sol
@@ -4,6 +4,21 @@ pragma solidity 0.8.6;
 import "@primitivefinance/v2-core/contracts/interfaces/callback/IPrimitiveSwapCallback.sol";
 
 interface ISwapManager is IPrimitiveSwapCallback {
+    /// @notice Thrown when the delta out is lower than the minimum
+    /// @param expected The minimum delta out
+    /// @param actual The actual delta out
+    error DeltaOutMinError(uint256 expected, uint256 actual);
+
+    event Swap(
+        address indexed recipient,
+        address indexed engine,
+        bytes32 indexed poolId,
+        bool riskyForStable,
+        uint256 deltaIn,
+        uint256 deltaOut,
+        bool fromMargin
+    );
+
     function swap(
         address engine,
         address risky,
@@ -14,5 +29,7 @@ interface ISwapManager is IPrimitiveSwapCallback {
         uint256 deltaOutMin,
         bool fromMargin,
         bool toMargin
-    ) external;
+    ) external returns (
+        uint256 deltaOut
+    );
 }
