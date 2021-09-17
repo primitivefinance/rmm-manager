79df1fa692bebf8d3f7ea629149d5e506d00e52c
Clement Lakhal <clementlakhal@gmail.com>
2021-09-17 09:50:21 +0400

    feat: add immutable and interface

--- contracts/base/HouseBase.sol
+++ contracts/base/HouseBase.sol
@@ -1,13 +1,25 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.6;
 
+import "../interfaces/IHouseBase.sol";
 import "../interfaces/IPrimitiveHouse.sol";
 
 import "./Reentrancy.sol";
 
 import "../libraries/EngineAddress.sol";
 
-abstract contract HouseBase is IPrimitiveHouse, Reentrancy {
-    /// @inheritdoc IPrimitiveHouse
-    address public override factory;
+abstract contract HouseBase is IHouseBase, Reentrancy {
+    /// @inheritdoc IHouseBase
+    address public immutable override factory;
+
+    /// @inheritdoc IHouseBase
+    address public immutable override WETH10;
+
+    constructor(
+        address _factory,
+        address _WETH10
+    ) {
+        factory = _factory;
+        WETH10 = _WETH10;
+    }
 }
