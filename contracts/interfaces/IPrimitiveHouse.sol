e8e4ce5e2b0cc7a9f9d0aea53f9d015f16296a71
Clement Lakhal <clementlakhal@gmail.com>
2021-09-20 21:03:12 +0400

    feat: update remove and natspec

--- contracts/interfaces/IPrimitiveHouse.sol
+++ contracts/interfaces/IPrimitiveHouse.sol
@@ -4,13 +4,30 @@ pragma solidity 0.8.6;
 import "@primitivefinance/v2-core/contracts/interfaces/callback/IPrimitiveCreateCallback.sol";
 import "@primitivefinance/v2-core/contracts/interfaces/callback/IPrimitiveLiquidityCallback.sol";
 
+/// @title IPrimitiveHouse
+/// @author Primitive
+/// @notice Interface of PrimitiveHouse
 interface IPrimitiveHouse is
     IPrimitiveCreateCallback,
     IPrimitiveLiquidityCallback
 {
+    /// ERRORS ///
+
+    /// @notice Emitted when the engine is not deployed
     error EngineNotDeployedError();
+
+    /// @notice Emitted when the liquidity is zero
     error ZeroLiquidityError();
 
+    /// EVENTS ///
+
+    /// @notice Emitted when a new pool is created
+    /// @param recipient The recipient of the liquidity
+    /// @param engine The address of the engine
+    /// @param poolId The id of the new pool
+    /// @param strike The strike of the new pool
+    /// @param sigma The sigma of the new pool
+    /// @param maturity The maturity of the new pool
     event Create(
         address indexed recipient,
         address indexed engine,
@@ -20,6 +37,14 @@ interface IPrimitiveHouse is
         uint32 maturity
     );
 
+    /// @notice Emitted when liquidity is allocated
+    /// @param payer The payer sending liquidity
+    /// @param engine The engine receiving liquidity
+    /// @param poolId The id of the pool receiving liquidity
+    /// @param delLiquidity The amount of liquidity allocated
+    /// @param delRisky The amount of risky tokens allocated
+    /// @param delStable The amount of stable tokens allocated
+    /// @param fromMargin True if liquidity was paid from margin
     event Allocate(
         address indexed payer,
         address indexed engine,
@@ -30,12 +55,18 @@ interface IPrimitiveHouse is
         bool fromMargin
     );
 
+    /// @notice Emitted when liquidity is removed
+    /// @param payer The payer receiving liquidity
+    /// @param engine The engine where liquidity is removed from
+    /// @param poolId The id of the pool where liquidity is removed from
+    /// @param delLiquidity The amount of liquidity removed
+    /// @param delRisky The amount of risky tokens allocated
+    /// @param delStable The amount of stable tokens allocated
     event Remove(
         address indexed payer,
         address indexed engine,
         bytes32 indexed poolId,
-        address risky,
-        address stable,
+        uint256 delLiquidity,
         uint256 delRisky,
         uint256 delStable
     );
@@ -65,6 +96,14 @@ interface IPrimitiveHouse is
         uint256 delStable
     );
 
+    /// @notice Allocates liquidity into a pool
+    /// @param engine The address of the engine
+    /// @param risky The address of the risky asset
+    /// @param stable The address of the stable asset
+    /// @param poolId The id of the pool
+    /// @param delLiquidity The amount of liquidity to allocate
+    /// @param fromMargin True if margins should be used
+    /// @param shouldTokenizeLiquidity True if liquidity should be tokenized
     function allocate(
         address engine,
         address risky,
@@ -78,10 +117,12 @@ interface IPrimitiveHouse is
         uint256 delStable
     );
 
+    /// @notice Removes liquidity from a pool
+    /// @param engine The address of the engine
+    /// @param poolId The id of the pool
+    /// @param delLiquidity The amount of liquidity to remove
     function remove(
         address engine,
-        address risky,
-        address stable,
         bytes32 poolId,
         uint256 delLiquidity
     ) external returns (
