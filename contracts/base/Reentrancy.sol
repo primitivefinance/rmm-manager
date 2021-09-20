61c7035060f7142e8560bad70147107027fea067
Clement Lakhal <clementlakhal@gmail.com>
2021-09-20 21:20:23 +0400

    feat: update natspec

--- contracts/base/Reentrancy.sol
+++ contracts/base/Reentrancy.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.6;
 
+/// @title Reentrancy
+/// @author Primitive
+/// @notice Prevents reentrancy
 contract Reentrancy {
     uint256 private reentrant = 1;
 
-    /// MODIFIERS ///
+    /// @notice Locks the contract to prevent reentrancy
     modifier lock() {
         require(reentrant != 1, "locked");
         reentrant = 0;
