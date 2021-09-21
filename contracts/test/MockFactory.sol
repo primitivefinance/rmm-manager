d1cb4603b66d4f48229fdba7beca3fabe47d8b90
Clement Lakhal <clementlakhal@gmail.com>
2021-09-21 20:27:03 +0400

    feat: update MockFactory

--- contracts/test/MockFactory.sol
+++ contracts/test/MockFactory.sol
@@ -5,19 +5,16 @@ import "@primitivefinance/v2-core/contracts/interfaces/IPrimitiveFactory.sol";
 import "./MockEngine.sol";
 
 contract MockFactory is IPrimitiveFactory {
+    /// @notice Thrown when the risky and stable tokens are the same
     error SameTokenError();
+
+    /// @notice Thrown when the risky or the stable token is 0x0...
     error ZeroAddressError();
 
-    /// @inheritdoc IPrimitiveFactory
-    uint256 public constant override MIN_LIQUIDITY_FACTOR = 6;
-    /// @inheritdoc IPrimitiveFactory
-    address public override owner;
-    mapping(address => mapping(address => address)) public override getEngine;
+    /// @notice Thrown on attempting to deploy an already deployed Engine
+    error DeployedError();
 
-    constructor() {
-        owner = msg.sender;
-    }
-
+    /// @notice Engine will use these variables for its immutable variables
     struct Args {
         address factory;
         address risky;
@@ -27,18 +24,54 @@ contract MockFactory is IPrimitiveFactory {
         uint256 minLiquidity;
     }
 
+    /// @inheritdoc IPrimitiveFactory
+    uint256 public constant override MIN_LIQUIDITY_FACTOR = 6;
+    /// @inheritdoc IPrimitiveFactory
+    address public override owner;
+    /// @inheritdoc IPrimitiveFactory
+    mapping(address => mapping(address => address)) public override getEngine;
+    /// @inheritdoc IPrimitiveFactory
     Args public override args; // Used instead of an initializer in Engine contract
 
+    constructor() {
+        owner = msg.sender;
+    }
+
+    /// @inheritdoc IPrimitiveFactory
     function deploy(address risky, address stable) external override returns (address engine) {
         if (risky == stable) revert SameTokenError();
         if (risky == address(0) || stable == address(0)) revert ZeroAddressError();
-        uint256 riskyDecimals = IERC20(risky).decimals();
-        uint256 stableDecimals = IERC20(stable).decimals();
+        if (getEngine[risky][stable] != address(0)) revert DeployedError();
+
+        engine = deploy(address(this), risky, stable);
+        getEngine[risky][stable] = engine;
+        emit Deployed(msg.sender, risky, stable, engine);
+    }
+
+    /// @notice         Deploys an engine contract with a `salt`.
+    /// @dev            Engine contract should have no constructor args, because this affects the deployed address
+    ///                 From solidity docs:
+    ///                 "It will compute the address from the address of the creating contract,
+    ///                 the given salt value, the (creation) bytecode of the created contract,
+    ///                 and the constructor arguments."
+    ///                 While the address is still deterministic by appending constructor args to a contract's bytecode,
+    ///                 it's not efficient to do so on chain.
+    /// @param  factory Address of the deploying smart contract
+    /// @param  risky   Risky token address, underlying token
+    /// @param  stable  Stable token address, quote token
+    /// @return engine  Engine contract address which was deployed
+    function deploy(
+        address factory,
+        address risky,
+        address stable
+    ) internal returns (address engine) {
+        (uint256 riskyDecimals, uint256 stableDecimals) = (IERC20(risky).decimals(), IERC20(stable).decimals());
         uint256 scaleFactorRisky = 10**(18 - riskyDecimals);
         uint256 scaleFactorStable = 10**(18 - stableDecimals);
-        uint256 minLiquidity = 10**((riskyDecimals > stableDecimals ? stableDecimals : riskyDecimals) / 6);
+        uint256 lowestDecimals = (riskyDecimals > stableDecimals ? stableDecimals : riskyDecimals);
+        uint256 minLiquidity = 10**(lowestDecimals / MIN_LIQUIDITY_FACTOR);
         args = Args({
-            factory: address(this),
+            factory: factory,
             risky: risky,
             stable: stable,
             scaleFactorRisky: scaleFactorRisky,
@@ -46,8 +79,6 @@ contract MockFactory is IPrimitiveFactory {
             minLiquidity: minLiquidity
         }); // Engines call this to get constructor args
         engine = address(new MockEngine{salt: keccak256(abi.encode(risky, stable))}());
-        getEngine[risky][stable] = engine;
-        emit Deployed(msg.sender, risky, stable, engine);
         delete args;
     }
 }
