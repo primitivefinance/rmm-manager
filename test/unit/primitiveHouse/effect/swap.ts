26e720dcf7900591c48f0ecd3db40e1d81903d8f
Clement Lakhal <clementlakhal@gmail.com>
2021-09-21 22:21:17 +0400

    test: update swap

--- test/unit/primitiveHouse/effect/swap.ts
+++ test/unit/primitiveHouse/effect/swap.ts
@@ -1,21 +1,44 @@
-import { ethers, waffle } from 'hardhat'
-import { expect } from 'chai'
+import { utils, constants } from 'ethers'
 import { parseWei } from 'web3-units'
 
-import loadContext, { DEFAULT_CONFIG } from '../../context'
+import { DEFAULT_CONFIG } from '../../context'
 import { computePoolId } from '../../../shared/utilities'
-import { swapFragment } from '../fragments'
+import expect from '../../../shared/expect'
+import { runTest } from '../../context'
 
-const { strike, sigma, maturity } = DEFAULT_CONFIG
+const { strike, sigma, maturity, delta } = DEFAULT_CONFIG
 let poolId: string
 
-describe('swap', function () {
-  before(async function () {
-    loadContext(waffle.provider, swapFragment)
-  })
-
+runTest('swap', function () {
   beforeEach(async function () {
+    await this.risky.mint(this.deployer.address, parseWei('1000000').raw)
+    await this.stable.mint(this.deployer.address, parseWei('1000000').raw)
+    await this.risky.approve(this.house.address, constants.MaxUint256)
+    await this.stable.approve(this.house.address, constants.MaxUint256)
+
+    await this.house.create(
+      this.engine.address,
+      this.risky.address,
+      this.stable.address,
+      strike.raw,
+      sigma.raw,
+      maturity.raw,
+      parseWei(delta).raw,
+      parseWei('1').raw,
+      false
+    )
+
     poolId = computePoolId(this.engine.address, strike.raw, sigma.raw, maturity.raw)
+
+    await this.house.allocate(
+      this.engine.address,
+      this.risky.address,
+      this.stable.address,
+      poolId,
+      parseWei('10').raw,
+      false,
+      false,
+    )
   })
 
   describe('success cases', function () {
@@ -23,6 +46,7 @@ describe('swap', function () {
       beforeEach(async function () {
         await this.house.deposit(
           this.deployer.address,
+          this.engine.address,
           this.risky.address,
           this.stable.address,
           parseWei('1000').raw,
@@ -31,20 +55,49 @@ describe('swap', function () {
       })
 
       it('swaps risky for stable', async function () {
-        await this.house.swap(
-          this.risky.address, this.stable.address, poolId, true, parseWei('1').raw, 0, true, true
-        )
+        await this.house.swap({
+          engine: this.engine.address,
+          risky: this.risky.address,
+          stable: this.stable.address,
+          poolId: poolId,
+          riskyForStable: true,
+          deltaIn: parseWei('1').raw,
+          deltaOutMin: 0,
+          fromMargin: true,
+          toMargin: true,
+          deadline: 1000000000000,
+        })
       })
 
       it('swaps stable for risky', async function () {
-        await this.house.swap(
-          this.risky.address, this.stable.address, poolId, false, parseWei('500').raw, 0, true, true
-        )
+        await this.house.swap({
+          engine: this.engine.address,
+          risky: this.risky.address,
+          stable: this.stable.address,
+          poolId: poolId,
+          riskyForStable: false,
+          deltaIn: parseWei('1').raw,
+          deltaOutMin: 0,
+          fromMargin: true,
+          toMargin: true,
+          deadline: 1000000000000,
+        })
       })
 
       it('emits the Swapped event', async function () {
         await expect(
-          this.house.swap(this.risky.address, this.stable.address, poolId, true, parseWei('1').raw, parseWei('1').raw, true, true)
+          this.house.swap({
+            engine: this.engine.address,
+            risky: this.risky.address,
+            stable: this.stable.address,
+            poolId: poolId,
+            riskyForStable: true,
+            deltaIn: parseWei('1').raw,
+            deltaOutMin: 0,
+            fromMargin: true,
+            toMargin: true,
+            deadline: 1000000000000,
+          })
         ).to.emit(this.house, 'Swap')
       })
     })
