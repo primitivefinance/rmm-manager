00ce9e2820f153eabe18d623e7fdd431e8fe3642
Clement Lakhal <clementlakhal@gmail.com>
2021-09-29 15:56:51 +0400

    style: rename constructor args

--- contracts/base/HouseBase.sol
+++ contracts/base/HouseBase.sol
@@ -23,13 +23,13 @@ abstract contract HouseBase is IHouseBase, Reentrancy {
     /// @inheritdoc IHouseBase
     address public immutable override WETH10;
 
-    /// @param _factory The address of a PrimitiveFactory
-    /// @param _WETH10 The address of WETH10
+    /// @param factory_ The address of a PrimitiveFactory
+    /// @param WETH10_ The address of WETH10
     constructor(
-        address _factory,
-        address _WETH10
+        address factory_,
+        address WETH10_
     ) {
-        factory = _factory;
-        WETH10 = _WETH10;
+        factory = factory_;
+        WETH10 = WETH10_;
     }
 }
