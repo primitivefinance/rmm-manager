5a33c3f4e915eaee0c932550b4a8e0a5c8794da0
Clement Lakhal <clementlakhal@gmail.com>
2021-10-01 22:16:48 +0200

    feat: wip swapExactOut

---
+++ test/unit/primitiveHouse/effect/swapExactOut.ts
@@ -0,0 +1,93 @@
+import { constants } from 'ethers'
+import { parseWei, Wei } from 'web3-units'
+
+import { DEFAULT_CONFIG } from '../../context'
+import { computePoolId } from '../../../shared/utilities'
+import expect from '../../../shared/expect'
+import { runTest } from '../../context'
+
+const { strike, sigma, maturity, delta } = DEFAULT_CONFIG
+let poolId: string
+let delLiquidity: Wei, delRisky: Wei, delStable: Wei
+
+runTest('swapExactOut', function () {
+  beforeEach(async function () {
+    await this.risky.mint(this.deployer.address, parseWei('1000000').raw)
+    await this.stable.mint(this.deployer.address, parseWei('1000000').raw)
+    await this.risky.approve(this.house.address, constants.MaxUint256)
+    await this.stable.approve(this.house.address, constants.MaxUint256)
+
+    await this.house.create(
+      this.risky.address,
+      this.stable.address,
+      strike.raw,
+      sigma.raw,
+      maturity.raw,
+      parseWei(delta).raw,
+      parseWei('1000').raw,
+    )
+
+    poolId = computePoolId(this.engine.address, strike.raw, sigma.raw, maturity.raw)
+
+/*
+
+    const amount = parseWei('1000')
+    const res = await this.engine.reserves(poolId)
+    delLiquidity = amount
+    delRisky = amount.mul(res.reserveRisky).div(res.liquidity)
+    delStable = amount.mul(res.reserveStable).div(res.liquidity)
+
+    await this.house.allocate(
+      poolId,
+      this.risky.address,
+      this.stable.address,
+      delRisky.raw,
+      delStable.raw,
+      false,
+    )
+
+    */
+  })
+
+  describe('success cases', function () {
+    describe('from margin / to margin', function () {
+      beforeEach(async function () {
+        await this.house.deposit(
+          this.deployer.address,
+          this.risky.address,
+          this.stable.address,
+          parseWei('100').raw,
+          parseWei('100').raw
+        )
+      })
+
+      it('swaps risky for stable', async function () {
+        /*
+        10000000000000000000
+        11074336834952110300
+
+        20000000000000000000
+        20867109361639727800
+
+        30000000000000000000
+        30676735352078302900
+
+
+        */
+
+        await this.house.swapExactOut({
+          recipient: this.deployer.address,
+          risky: this.risky.address,
+          stable: this.stable.address,
+          poolId: poolId,
+          riskyForStable: true,
+          deltaOut: parseWei('100').raw,
+          deltaInMax: parseWei('1000000').raw,
+          fromMargin: false,
+          toMargin: true,
+          deadline: 1000000000000,
+        })
+      })
+    })
+  })
+})
