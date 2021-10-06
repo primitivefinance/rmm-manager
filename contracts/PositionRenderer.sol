011fec3dd84e6e99d7487ca6d99f6403dcf2a395
Clement Lakhal <clementlakhal@gmail.com>
2021-10-06 03:07:24 +0200

    feat: wip PositionRenderer

--- contracts/PositionRenderer.sol
+++ contracts/PositionRenderer.sol
@@ -6,6 +6,33 @@ import "@primitivefinance/v2-core/contracts/interfaces/engine/IPrimitiveEngineVi
 import "./interfaces/IPositionRenderer.sol";
 import "./interfaces/IERC20WithOptions.sol";
 
+library HexStrings {
+    bytes16 internal constant ALPHABET = '0123456789abcdef';
+
+    /// @notice Converts a `uint256` to its ASCII `string` hexadecimal representation with fixed length.
+    /// @dev Credit to Open Zeppelin under MIT license https://github.com/OpenZeppelin/openzeppelin-contracts/blob/243adff49ce1700e0ecb99fe522fb16cff1d1ddc/contracts/utils/Strings.sol#L55
+    function toHexString(uint256 value, uint256 length) internal pure returns (string memory) {
+        bytes memory buffer = new bytes(2 * length + 2);
+        buffer[0] = '0';
+        buffer[1] = 'x';
+        for (uint256 i = 2 * length + 1; i > 1; --i) {
+            buffer[i] = ALPHABET[value & 0xf];
+            value >>= 4;
+        }
+        require(value == 0, 'Strings: hex length insufficient');
+        return string(buffer);
+    }
+
+    function toHexStringNoPrefix(uint256 value, uint256 length) internal pure returns (string memory) {
+        bytes memory buffer = new bytes(2 * length);
+        for (uint256 i = buffer.length; i > 0; i--) {
+            buffer[i - 1] = ALPHABET[value & 0xf];
+            value >>= 4;
+        }
+        return string(buffer);
+    }
+}
+
 contract PositionRenderer is IPositionRenderer {
     function uri(
         address engineAddress,
@@ -33,6 +60,30 @@ contract PositionRenderer is IPositionRenderer {
 
     function renderSVG(uint256 tokenId) public view returns (string memory) {
         return "";
+    }
+
+    function returnGradient(
+        address risky,
+        address stable
+    ) public pure returns (string memory) {
+        return string(
+            abi.encodePacked(
+                '<defs><linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="100%">',
+                '<stop offset="0%" style="stop-color:',
+                returnColor(risky),
+                ';stop-opacity:1" /><stop offset="100%" style="stop-color:',
+                returnColor(stable),
+                ';stop-opacity:1" /></linearGradient></defs>'
+            )
+        );
+    }
+    function returnColor(address token) public pure returns (string memory) {
+        return string(
+            abi.encodePacked(
+                "#",
+                HexStrings.toHexStringNoPrefix(uint160(token), 3)
+            )
+        );
     }
 
     function returnText(
