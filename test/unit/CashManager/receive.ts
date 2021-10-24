3829515d09cab775c5b2fb0b5c5fab392c2f752e
Clement Lakhal <clementlakhal@gmail.com>
2021-10-24 16:51:38 +0200

    test: add receive test

---
+++ test/unit/CashManager/receive.ts
@@ -0,0 +1,17 @@
+import { parseWei } from 'web3-units'
+
+import expect from '../../shared/expect'
+import { runTest } from '../context'
+
+runTest('receive', function () {
+  describe('fail cases', function () {
+    it('fails to receive ETH if the sender is not WETH', async function () {
+      await expect(
+        this.deployer.sendTransaction({
+          to: this.house.address,
+          value: parseWei('1').raw,
+        }),
+      ).to.be.reverted
+    })
+  })
+})
