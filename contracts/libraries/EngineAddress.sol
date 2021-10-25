b2fa645075b7be60d2df0334bcb18609963ca913
Clement Lakhal <clementlakhal@gmail.com>
2021-10-25 23:34:26 +0200

    feat: change Solidity requirement for interfaces

--- contracts/libraries/EngineAddress.sol
+++ contracts/libraries/EngineAddress.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-only
-pragma solidity 0.8.6;
+pragma solidity >=0.8.6;
 
 /// @author  Primitive
 /// @notice  Small library to compute the address of the engines
