1c7d4da8efe1e64c34dde9d43ab1fe3fb897a686
clemlak <clementlakhal@gmail.com>
2021-10-26 19:58:10 +0200

    feat: update NatSpec

--- contracts/base/Reentrancy.sol
+++ contracts/base/Reentrancy.sol
@@ -1,17 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.6;
 
-/// @title   Reentrancy
+/// @title   Reentrancy contract
 /// @author  Primitive
 /// @notice  Prevents reentrancy
 
 contract Reentrancy {
-    /// @notice Thrown when a call to the contract is made during a locked state
+    /// @notice  Thrown when a call to the contract is made during a locked state
     error LockedError();
 
     uint256 private _unlocked = 1;
 
-    /// @notice Locks the contract to prevent reentrancy
+    /// @notice  Locks the contract to prevent reentrancy
     modifier lock() {
         if (_unlocked != 1) revert LockedError();
 
