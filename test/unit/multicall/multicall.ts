7fc230d3218b08a4e03e9d5928940240bbae54a1
clemlak <clementlakhal@gmail.com>
2021-10-26 19:38:03 +0200

    test: remove useless imports

--- test/unit/multicall/multicall.ts
+++ test/unit/multicall/multicall.ts
@@ -1,7 +1,4 @@
-import hre, { ethers } from 'hardhat'
 import { parseWei } from 'web3-units'
-
-import expect from '../../shared/expect'
 import { runTest } from '../context'
 
 const value = parseWei('1').raw
@@ -9,14 +6,12 @@ const value = parseWei('1').raw
 runTest('multicall', function () {
   describe('success cases', function () {
     it('batches multiple calls into one', async function () {
-      const wrapData = this.house.interface.encodeFunctionData('wrap', [value.div(2)]);
-      const wrapData2 = this.house.interface.encodeFunctionData('wrap', [value.div(2)]);
+      const wrapData = this.house.interface.encodeFunctionData('wrap', [value.div(2)])
+      const wrapData2 = this.house.interface.encodeFunctionData('wrap', [value.div(2)])
 
-      await this.house.multicall(
-        [wrapData, wrapData2], {
-          value,
-        }
-      )
+      await this.house.multicall([wrapData, wrapData2], {
+        value,
+      })
     })
   })
 })
