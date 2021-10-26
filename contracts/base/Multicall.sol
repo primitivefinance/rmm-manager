0efa6e0d846bee8d7d5d7f403a101b3e0ff9467b
clemlak <clementlakhal@gmail.com>
2021-10-26 19:37:49 +0200

    feat: update NatSpec

--- contracts/base/Multicall.sol
+++ contracts/base/Multicall.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.6;
 
-/// @title   Multicall
+/// @title   Multicall contract
 /// @author  https://github.com/Uniswap/v3-periphery/blob/main/contracts/base/Multicall.sol
 /// @notice  Enables calling multiple methods in a single call to the contract
 
