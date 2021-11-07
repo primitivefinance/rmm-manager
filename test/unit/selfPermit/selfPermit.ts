e23e48caa8ec8538b94ff8205ebe5babd5903159
Clement Lakhal <clementlakhal@gmail.com>
2021-11-07 02:39:11 +0100

    test: add selfPermit test

--- test/unit/selfPermit/selfPermit.ts
+++ test/unit/selfPermit/selfPermit.ts
@@ -1,74 +1,89 @@
 import { parseWei } from 'web3-units'
-import { TypedDataDomain, TypedDataField } from '@ethersproject/abstract-signer'
-import { utils } from 'ethers'
 import expect from '../../shared/expect'
+import getPermitSignature from '../../shared/permit'
 import { runTest } from '../context'
 
 const value = parseWei('1').raw
+const deadline = 999999999999
 
 runTest('selfPermit', function () {
   describe('success cases', function () {
-    it('wraps ETH into WETH', async function () {
-      const domain: TypedDataDomain = {
-        name: 'TestToken',
-        version: '1',
-      }
-
-      const Approve: TypedDataField[] = [
-        {
-          name: 'owner',
-          type: 'address',
-        },
-        {
-          name: 'spender',
-          type: 'address',
-        },
-        {
-          name: 'value',
-          type: 'uint256',
-        },
-        {
-          name: 'nonce',
-          type: 'uint256',
-        },
-        {
-          name: 'deadline',
-          type: 'uint256',
-        },
-      ]
-
-      const types: Record<string, TypedDataField[]> = {
-        Approve
-      }
-
-      const values: Record<string, any> = {
-        owner: this.deployer.address,
-        spender: this.house.address,
-        value,
-        nonce: 0,
-        deadline: 999999,
-      }
-
-      const sig = await this.deployer._signTypedData(domain, types, values)
-      const splitSig = utils.splitSignature(sig);
-
-      await this.house.selfPermit(
+    it('self approves using the signature', async function () {
+      const signature = await getPermitSignature(
+        this.deployer,
         this.risky.address,
         this.house.address,
         value,
-        splitSig.v,
-        splitSig.r,
-        splitSig.s
+        deadline, {
+          name: 'TestToken',
+          nonce: '0',
+          version: '1',
+          chainId: await this.deployer.getChainId(),
+        }
+      )
+
+      await this.house.selfPermit(
+        this.risky.address,
+        value,
+        deadline,
+        signature.v,
+        signature.r,
+        signature.s,
       )
 
       expect(
-        this.risky.allowance(this.deployer.address, this.house.address)
-      )
+        await this.risky.allowance(this.deployer.address, this.house.address)
+      ).to.be.equal(value)
     })
   })
 
   describe('fail cases', function () {
-    it('fails to wrap if not enough value is sent', async function () {
+    it('reverts if the deadline is reached', async function () {
+      const signature = await getPermitSignature(
+        this.deployer,
+        this.risky.address,
+        this.house.address,
+        value,
+        0, {
+          name: 'TestToken',
+          nonce: '0',
+          version: '1',
+          chainId: await this.deployer.getChainId(),
+        }
+      )
+
+      await expect(this.house.selfPermit(
+        this.risky.address,
+        value,
+        0,
+        signature.v,
+        signature.r,
+        signature.s,
+      )).to.be.revertedWith('ERC20Permit: expired deadline')
+    })
+
+    it('reverts if the signature is invalid', async function () {
+      const signature = await getPermitSignature(
+        this.deployer,
+        this.risky.address,
+        this.house.address,
+        value,
+        deadline, {
+          name: 'TestToken',
+          nonce: '0',
+          version: '1',
+          chainId: await this.deployer.getChainId(),
+        }
+      )
+
+      await expect(this.house.selfPermit(
+        this.risky.address,
+        value.mul(2),
+        deadline,
+        signature.v,
+        signature.r,
+        signature.s,
+      )).to.be.revertedWith('ERC20Permit: invalid signature')
     })
   })
 })
