638957c0b3057955bc5d45ecf729bfff790d1629
Clement Lakhal <clementlakhal@gmail.com>
2021-11-26 01:17:01 +0400

    feat: update NatSpec

--- contracts/base/ERC1155Permit.sol
+++ contracts/base/ERC1155Permit.sol
@@ -1,19 +1,25 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.6;
 
-import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+/// @title   ERC1155Permit contract
+/// @author  Primitive
+/// @notice  ERC1155 contract with permit extension allowing approvals to be made via signatures
 
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "./ERC1155.sol";
 import "../interfaces/IERC1155Permit.sol";
 
 contract ERC1155Permit is ERC1155, IERC1155Permit, EIP712 {
+    /// @inheritdoc IERC1155Permit
     mapping(address => uint256) public override nonces;
 
     bytes32 private immutable _PERMIT_TYPEHASH =
         keccak256("Permit(address owner,address spender,uint256 id,uint256 amount,bytes memory data,uint256 deadline,uint8 v,bytes32 r,bytes32 s");
 
+    /// @param name Name of the contract
     constructor(string memory name) ERC1155("") EIP712(name, "1") { }
 
+    /// @inheritdoc IERC1155Permit
     function permit(
         address owner,
         address operator,
@@ -43,6 +49,7 @@ contract ERC1155Permit is ERC1155, IERC1155Permit, EIP712 {
         _setApprovalForAll(owner, operator, approved);
     }
 
+    /// @inheritdoc IERC1155Permit
     function DOMAIN_SEPARATOR() external view override returns (bytes32) {
         return _domainSeparatorV4();
     }
