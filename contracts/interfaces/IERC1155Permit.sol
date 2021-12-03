9ef4c2153d68727db9d2c3af1d1632a440b95e72
Clement Lakhal <clementlakhal@gmail.com>
2021-12-03 18:25:12 +0400

    feat: add IERC1155 to IERC1155Permit

--- contracts/interfaces/IERC1155Permit.sol
+++ contracts/interfaces/IERC1155Permit.sol
@@ -4,7 +4,9 @@ pragma solidity >=0.8.6;
 /// @title   Interface of ERC1155Permit contract
 /// @author  Primitive
 
-interface IERC1155Permit {
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+
+interface IERC1155Permit is IERC1155 {
     /// ERRORS ///
 
     /// @notice Thrown when the signature has expired
