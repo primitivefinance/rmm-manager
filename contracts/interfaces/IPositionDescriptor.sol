b513daa1de2dcde2a1be4c7fa516cd233b3a1c24
Lint Action <lint-action@samuelmeuli.com>
2021-12-26 10:08:30 +0000

    Fix code style issues with Prettier

--- contracts/interfaces/IPositionDescriptor.sol
+++ contracts/interfaces/IPositionDescriptor.sol
@@ -4,7 +4,6 @@ pragma solidity >=0.8.6;
 /// @title   Interface of PositionDescriptor contract
 /// @author  Primitive
 interface IPositionDescriptor {
-
     function positionRenderer() external view returns (address);
 
     function getMetadata(address engine, uint256 tokenId) external view returns (string memory);
