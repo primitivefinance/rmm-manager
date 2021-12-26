a89b79b5aafdfe878b5096f7362195ba72ab752d
Clement Lakhal <clementlakhal@gmail.com>
2021-12-26 14:49:39 +0400

    feat: update NatSpec

--- contracts/interfaces/IPositionDescriptor.sol
+++ contracts/interfaces/IPositionDescriptor.sol
@@ -4,8 +4,13 @@ pragma solidity >=0.8.6;
 /// @title   Interface of PositionDescriptor contract
 /// @author  Primitive
 interface IPositionDescriptor {
+    /// VIEW FUNCTIONS ///
 
+    /// @notice  Returns the PositionRenderer contract
     function positionRenderer() external view returns (address);
 
+    /// @notice         Returns the metadata of a token
+    /// @param tokenId  Id of the token (same as pool id)
+    /// @return         JSON metadata of the token
     function getMetadata(address engine, uint256 tokenId) external view returns (string memory);
 }
