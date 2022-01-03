4bd92f45c4c570196c28f2939279e3ddc59b4cab
clemlak <clementlakhal@gmail.com>
2022-01-03 18:38:53 +0400

    test: fix ManagerBase constructor test

--- test/unit/managerBase/constructor.ts
+++ test/unit/managerBase/constructor.ts
@@ -7,7 +7,7 @@ runTest('constructor', function () {
     it('fetches the variables set by the constructor', async function () {
       expect(await this.manager.factory()).to.be.equal(this.factory.address)
       expect(await this.manager.WETH9()).to.be.equal(this.weth.address)
-      expect(await this.manager.positionRenderer()).to.be.equal(this.positionRenderer.address)
+      expect(await this.manager.positionDescriptor()).to.be.equal(this.positionDescriptor.address)
     })
   })
 
