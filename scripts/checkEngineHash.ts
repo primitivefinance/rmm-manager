34a1ed72e98c7e108352da70fe12e15db6309dd4
clemlak <clementlakhal@gmail.com>
2022-03-02 11:30:26 +0400

    feat: turn checkEngineBytecodeHash into a task

--- scripts/checkEngineHash.ts
+++ scripts/checkEngineHash.ts
@@ -1,25 +1,38 @@
-import hre from 'hardhat'
+import { task, types } from 'hardhat/config';
 import { utils } from 'ethers'
 import { bytecode } from '@primitivefi/rmm-core/artifacts/contracts/PrimitiveEngine.sol/PrimitiveEngine.json';
+import fs from 'fs';
 
-async function main() {
-  const hash = utils.keccak256(bytecode)
+task('checkEngineBytecodeHash', 'Checks if the engine bytecode hashes are matching')
+  .addOptionalPositionalParam('update', 'Updates the bytecode hash of the Manager', false, types.boolean)
+  .setAction(async (args, hre) => {
+    await hre.run('compile')
 
-  const TestHash = await hre.ethers.getContractFactory('TestHash');
-  const testHash = await TestHash.deploy();
+    const hash = utils.keccak256(bytecode)
 
-  const savedHash = await testHash.getEngineCodeHash();
+    const TestHash = await hre.ethers.getContractFactory('TestHash');
+    const testHash = await TestHash.deploy();
 
-  if (savedHash === hash) {
-    console.log('✅ Engine bytecode hash is correct!');
-  } else {
-    console.log('❌ Engine bytecode hash is incorrect!');
-  }
-}
+    const savedHash = await testHash.getEngineCodeHash();
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error)
-    process.exit(1)
-  })
+    if (savedHash === hash) {
+      console.log('✅ Engine bytecode hash is correct!');
+    } else {
+      console.log('❌ Engine bytecode hash is incorrect!');
+
+      if (args.update) {
+        let source = await fs.promises.readFile('./contracts/libraries/EngineAddress.sol', {
+          encoding: 'utf-8',
+        });
+
+        source = source.replace(savedHash, hash);
+        await fs.promises.writeFile(
+          './contracts/libraries/EngineAddress.sol',
+          source, {
+          encoding: 'utf-8',
+        });
+
+        console.log('✅ Engine bytecode hash updated!');
+      }
+    }
+  });
